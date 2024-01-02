60cb46fc504bd8b73b55f5ea9e3e6438d69382b3
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2024-01-02 10:43:55 +0530

    Worked on pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
+
 
 function App() {
   return (
     <>
-      <h1 className="">Hello World !</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sign-in" element={<Signin />}/>
+          <Route path="/sign-up" element={<SignUp />}/>
+          <Route path="/forgot-password" element={<ForgotPassword />}/>
+          <Route path="/offers" element={<Offers />}/>
+        </Routes>
+      </Router>
     </>
   );
 }
