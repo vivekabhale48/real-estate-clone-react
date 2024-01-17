c1dc5b5f9a5391fc52d5dbce76caa3263a4e8978
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2024-01-17 17:29:18 +0530

    Created private route for protecting the profile page and logout functionality
    Also created the custom hook - useAuthStatus.jsx which gives us the status whether the user is logged in or not.

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router';
+import { useAuthStatus } from '../customHooks/useAuthStatus';
+
+const PrivateRoute = () => {
+    const {isLoggedIn, checkingStatus} = useAuthStatus();
+
+    //Here if we removed the below line then the authentication wont work, even tough we are sign in and try to hit the profile route we would get redirected to the signin route because the page of react would load before useAuthStatus hook. So now because of line 9 or below line it is checking the status so the loading is shown on the screen until the status loggedin is being checked. After that the next 10th line is executed.
+    if(checkingStatus) return <h3>Loading....</h3>
+    return isLoggedIn ? <Outlet /> : <Navigate to="/sign-in" />
+}
+
+export default PrivateRoute
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-export default Profile
+  function onChange(e) {
+    setFormData((preval)=> ({
+      ...preval,
+       [e.target.id]: e.target.value
+    }))
+  }
+
+  const { name, email } = formData;
+
+  function signOut() {
+    auth.signOut();
+    navigate("/");
+  }
+
+  return (
+    <>
+      <section className="mt-6 xl:max-w-[1280px] lg:max-w-[1024px] md:max-w-[768px] mx-auto">
+        <h1 className="text-3xl font-bold text-center mt-6">My Profile</h1>
+        <div className="mt-6 px-3 flex flex-col justify-center items-center">
+          <form action="" className="w-full flex flex-col items-center">
+            <input
+              type="text"
+              className="md:w-[50%] md:mb-6 max-md:mb-4 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+              id="name"
+              value={name}
+              onChange={onChange}
+              disabled
+            />
+            <input
+              type="email"
+              className="md:w-[50%] md:mb-6 max-md:mb-4 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+              id="email"
+              value={email}
+              onChange={onChange}
+              disabled
+            />
+
+            <div className="w-full md:w-[50%] flex justify-between whitespace-nowrap text-sm sm:text-lg md:mb-6 max-md:mb-4">
+              <p className="flex items-center">Do you want to change your name ?
+                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
+              </p>
+              <p onClick={signOut} className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer">Sign out</p>
+          
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Profile;
