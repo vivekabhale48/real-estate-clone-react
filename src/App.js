8f6c57a315012976b66e1152ec894b69c3ad90c3
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2024-01-25 00:28:11 +0530

    Added functionality of onchange and onsubmit functions to the create listing page
    Also added the functionality to store the images to firebase and get return the image urls along will all the form details.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 
@@ -18,14 +18,16 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<PrivateRoute />} >
+          <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,18 @@
 import { type } from "@testing-library/user-event/dist/type";
 import React, { useState } from "react";
-
+import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
+import { getAuth } from "firebase/auth";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 const CreateListing = () => {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [geolocationEnabled, setGeolocation] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -14,6 +25,9 @@ const CreateListing = () => {
     offer: true,
     regularPrice: 1,
     discountedPrice: 1,
+    latitude: 0,
+    longitude: 0,
+    images: {},
   });
   const {
     type,
@@ -27,13 +41,152 @@ const CreateListing = () => {
     offer,
     regularPrice,
     discountedPrice,
+    latitude,
+    longitude,
+    images,
   } = formData;
-  const onChange = () => {};
+  const onChange = (e) => {
+    let boolean = null;
+    //This if else block is written to convert the string true false to boolean true false. Otherwise it wont works
+    if (e.target.value === "true") {
+      boolean = true;
+    } else if (e.target.value === "false") {
+      boolean = false;
+    } else if (e.target.files) {
+      setFormData((previousValue) => ({
+        ...previousValue,
+        images: e.target.files,
+      }));
+    }
+    if (!e.target.files) {
+      setFormData((previousValue) => ({
+        ...previousValue,
+        [e.target.id]: boolean ?? e.target.value, // ?? this symbol means if the boolean is not null, true or false then e.target.value will work.
+      }));
+    }
+  };
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    console.log([...images]);
+    setLoading(true);
+    if (+discountedPrice >= +regularPrice) {
+      setLoading(false);
+      toast.error("Discounted Price must be less than Regular Price");
+      return;
+    }
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Maximum 6 images are only allowed");
+      return;
+    }
+    let geolocation = {};
+    let location;
+    //To enable geo location go to google cloud console website select your working project search for geocoding api and enable it. This will provide you with some API key.
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      //Some times when address is not correct the results are zero hence put ? after results and if both are not there i.e null or undefined then put lat as zero hence ??.
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location = data.status === "ZERO_RESULTS" && undefined; //It means that if data.status is ZERO_RESULTS then make location = undefined.
+
+      if (location === undefined) {
+        setLoading(false);
+        toast.error("Please enter a correct address.");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        //The usage or how to upload files to firebase is given on their site.
+        const storage = getStorage();
+        //If the same person add same images two times so to give unique file name every time we are using a package called uuid. This is how you call uuidv4 you can checkout its usage in npm.
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+            reject(error)
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log("File available at", downloadURL);
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+
+    //Promise.all is used when there are multiple promises to be handled simultaneously. It in takes the array of promises and then resolves it. The below function calls the storeImage function for every image and it returns the array of uploaded image urls which it gets from the storeImage function.
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((e) => {
+      setLoading(false);
+      toast.error("Error occurred while uploading images.");
+      return;
+    });
+
+    console.log(imgUrls);
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    }
+
+    delete formDataCopy.images;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    delete formDataCopy.latitude;
+    delete formDataCopy.longitude;
+
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    setLoading(false);
+    toast.success("Listing created Successfully!")
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+  }
+
+  if (loading) return <Spinner />;
+
   return (
     <section className="mt-6 xl:max-w-[1280px] lg:max-w-[1024px] md:max-w-[768px] mx-auto">
       <h1 className="text-3xl font-bold text-center mt-6">My Profile</h1>
       <div className="mt-6 px-3 flex flex-col justify-center max-w-[740px] mx-auto">
-        <form action="">
+        <form onSubmit={onSubmit}>
           <div className="md:mb-6 max-md:mb-4">
             <label className=" md:mb-6 max-md:mb-4 font-semibold text-lg">
               Sell/Rent
@@ -42,10 +195,10 @@ const CreateListing = () => {
               <button
                 type="button"
                 id="type"
-                value="sale"
+                value="sell"
                 onClick={onChange}
                 className={`px-7 py-3 font-medium text-sm uppercase shadow-md hover:shadow-lg rounded focus:shadow-lg w-full active:shadow-lg transition duration-150 ease-in-out ${
-                  type === "sell" ? "bg-white" : "bg-slate-600 text-white"
+                  type === "rent" ? "bg-white" : "bg-slate-600 text-white"
                 }`}
               >
                 sell
@@ -53,10 +206,10 @@ const CreateListing = () => {
               <button
                 type="button"
                 id="type"
-                value="sale"
+                value="rent"
                 onClick={onChange}
                 className={`px-7 py-3 font-medium text-sm uppercase shadow-md hover:shadow-lg rounded focus:shadow-lg w-full active:shadow-lg transition duration-150 ease-in-out ${
-                  type === "rent" ? "bg-white" : "bg-slate-600 text-white"
+                  type === "sell" ? "bg-white" : "bg-slate-600 text-white"
                 }`}
               >
                 Rent
@@ -70,9 +223,9 @@ const CreateListing = () => {
             <div className="flex gap-x-6">
               <input
                 type="text"
-                id="type"
+                id="name"
                 value={name}
-                onClick={onChange}
+                onChange={onChange}
                 placeholder="Name"
                 maxLength="32"
                 minLength="10"
@@ -98,7 +251,7 @@ const CreateListing = () => {
               <p className="text-lg font-semibold">Bathroom</p>
               <input
                 type="number"
-                id="bathroom"
+                id="bathrooms"
                 value={bathrooms}
                 onChange={onChange}
                 min="1"
@@ -174,13 +327,41 @@ const CreateListing = () => {
                 type="text"
                 id="address"
                 value={address}
-                onClick={onChange}
+                onChange={onChange}
                 placeholder="Address"
                 required
                 className={`px-4 py-2 font-medium text-xl text-gray-700 border-gray-300 bg-white shadow-md hover:shadow-lg rounded focus:shadow-lg w-full active:shadow-lg transition duration-150 ease-in-out focus:border-slate-600`}
               />
             </div>
           </div>
+          {!geolocationEnabled && (
+            <div className="flex items-center gap-x-3 md:mb-6 max-md:mb-4">
+              <div>
+                <p className="text-lg font-semibold">Latitude</p>
+                <input
+                  type="number"
+                  id="latitude"
+                  value={latitude}
+                  onChange={onChange}
+                  min="-90"
+                  max="90"
+                  className="px-7 py-3 font-medium text-sm uppercase shadow-md hover:shadow-lg rounded focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out text-center"
+                />
+              </div>
+              <div>
+                <p className="text-lg font-semibold">Longitude</p>
+                <input
+                  type="number"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onChange}
+                  min="-180"
+                  max="180"
+                  className="px-7 py-3 font-medium text-sm uppercase shadow-md hover:shadow-lg rounded focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out text-center"
+                />
+              </div>
+            </div>
+          )}
           <div className="md:mb-6 max-md:mb-4">
             <label className=" md:mb-6 max-md:mb-4 font-semibold text-lg">
               Description
@@ -190,7 +371,7 @@ const CreateListing = () => {
                 type="text"
                 id="description"
                 value={description}
-                onClick={onChange}
+                onChange={onChange}
                 placeholder="Description"
                 required
                 className={`px-4 py-2 font-medium text-xl text-gray-700 border-gray-300 bg-white shadow-md hover:shadow-lg rounded focus:shadow-lg w-full active:shadow-lg transition duration-150 ease-in-out focus:border-slate-600`}
@@ -261,23 +442,25 @@ const CreateListing = () => {
           )}
           <div className="md:mb-6 max-md:mb-4">
             <p className="text-lg font-semibold">Images</p>
-            <p className="text-gray-600">The first Image will be the cover (max 6)</p>
+            <p className="text-gray-600">
+              The first Image will be the cover (max 6)
+            </p>
             <input
               type="file"
               id="images"
-              accept=".jpg,.png,.jpeg"
               onChange={onChange}
+              accept=".jpg,.png,.jpeg"
               multiple
               required
               className="w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:border-slate-600"
             />
           </div>
           <button
-          type="submit"
-          className="mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-        >
-          Create Listing
-        </button>
+            type="submit"
+            className="mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Create Listing
+          </button>
         </form>
       </div>
     </section>
