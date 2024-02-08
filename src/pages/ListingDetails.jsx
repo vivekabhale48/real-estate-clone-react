390e676de65289c4abb18c6c6ccf6d184b736305
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2024-02-09 00:33:26 +0530

    Add the listing information to the listing page

--- src/pages/ListingDetails.jsx
+++ src/pages/ListingDetails.jsx
@@ -8,6 +8,8 @@ import "swiper/css/bundle";
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper/modules";
 import { PiShareFatFill } from "react-icons/pi";
 import { toast } from "react-toastify";
+import { FaBath, FaBed, FaChair } from "react-icons/fa6";
+import { FaMapMarkerAlt, FaParking } from "react-icons/fa";
 
 const ListingDetails = () => {
   const params = useParams();
@@ -57,6 +59,45 @@ const ListingDetails = () => {
           navigator.clipboard.writeText(window.location.href);
           toast.success("Link copied to Clipboard!")
         }}/>
+      </div>
+
+      <div className="flex flex-col md:flex-row mx-auto max-w-6xl m-4 p-4 rounded-lg shadow-lg bg-white">
+        <div className="w-full">
+          <p className="text-2xl font-bold mb-3 text-blue-900">{listing.name} - ${listing.offer ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold"><FaMapMarkerAlt className="text-green-700 mr-1" />{listing.address}</p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "Rent" : "Sale"}
+            </p>
+            {listing.offer && (
+              <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+                ${+listing.regularPrice - +listing.discountedPrice} discount
+              </p>
+            )}
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">Description - </span>
+            {listing.description}
+          </p>
+          <div className="flex justify-between items-center">
+            <span className="flex justify-between items-center"><FaBed className="mr-2"/> {listing.bedrooms} Beds</span>
+            <span className="flex justify-between items-center"><FaBath className="mr-2"/> {listing.bathrooms} Baths</span>
+            {
+              listing.furnished && (
+                <span className="flex justify-between items-center"><FaChair className="mr-2"/> Furnished</span>
+              )
+            }
+            {
+              listing.parking && (
+                <span className="flex justify-between items-center"><FaParking className="mr-2"/> Parking Spot</span>
+              )
+            }
+          </div>
+        </div>
+        <div className="w-full">
+
+        </div>
       </div>
     </main>
   );
